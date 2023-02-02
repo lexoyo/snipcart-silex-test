b4e0b335c6f35ea1a3cb93fd3653279c4aee0cea
Alex Hoyau <a.hoyau@silexlabs.org>
2023-02-02 12:00:12 +0100

    Batch update

---
+++ js/script.js
@@ -0,0 +1,4 @@
+
+
+    window.silex = window.silex || {}
+    window.silex.data = {"site":{"width":980},"pages":[{"id":"page-home","displayName":"Home","link":{"linkType":"LinkTypePage","href":"#!page-home"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-contact","displayName":"Contact","link":{"linkType":"LinkTypePage","href":"#!page-contact"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false}]}
